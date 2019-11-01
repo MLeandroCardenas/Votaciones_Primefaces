831f46a8fbe40c0a9d899a50a4450a28b53e5dee
MLeandroCardenas <mleandrocardenas@ucundinamarca.edu.co>
2019-11-01 00:38:19 -0500

    Completo

--- src/main/java/com/udec/controlador/Votaciones.java
+++ src/main/java/com/udec/controlador/Votaciones.java
@@ -8,19 +8,29 @@ package com.udec.controlador;
 import com.udec.modelo.LogicaVotaciones;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.PrimeFaces;
 import org.primefaces.model.chart.PieChartModel;
 
 /**
  *
- * @author michl
+ * @author Michael Cardenas
+ * controlador de la vista 
  */
 @Named(value = "votaciones")
 @ViewScoped
 public class Votaciones implements Serializable {
     
+    /**
+     * variables para llevar el conteo de votos de cada candidato
+     */
     private int contadorUno,contadorDos,contadorTres;
+    
+    /**
+     * variable para crear el grafico y llenarlo
+     */
     private PieChartModel grafica;
 
     /**
@@ -29,6 +39,9 @@ public class Votaciones implements Serializable {
     public Votaciones() {
     }
     
+    /**
+     * metodo para inciar las variables apenas arranque el programa
+     */
     @PostConstruct
     public void init(){
         contadorUno=0;
@@ -36,6 +49,10 @@ public class Votaciones implements Serializable {
         contadorTres=0;
     }
     
+    /**
+     * metodo que incrementa de a uno el voto del candidato uno
+     * @return  el valor actual para verlo en la vista al usuario
+     */
     public int votosCandidatoUno(){
         contadorUno++;
         setContadorUno(contadorUno);
@@ -43,7 +60,10 @@ public class Votaciones implements Serializable {
         return getContadorUno();
     }
     
-    
+    /**
+     * metodo que incrementa de a uno el voto del candidato dos
+     * @return  el valor actual para verlo en la vista al usuario
+     */
     public int votosCandidatoDos(){
         contadorDos++;
         setContadorDos(contadorDos);
@@ -51,6 +71,10 @@ public class Votaciones implements Serializable {
         return getContadorDos();
     }
     
+    /**
+     * metodo que incrementa de a uno el voto del candidato tres
+     * @return  el valor actual para verlo en la vista al usuario
+     */
     public int votosCandidatoTres(){
         contadorTres++;
         setContadorTres(contadorTres);
@@ -58,6 +82,9 @@ public class Votaciones implements Serializable {
         return getContadorTres();
     }
     
+    /**
+     * metodo que crea la grafica con los datos actuales
+     */
     private void crearGrafica(){
         grafica = new PieChartModel();
         grafica.set("Alvaro Fajardo",getContadorUno());
@@ -71,11 +98,55 @@ public class Votaciones implements Serializable {
         grafica.setDiameter(150);
         grafica.setShadow(false);
     }
+    
+    /**
+     * limpia la grafica cuando finaliza la votacion
+     */
+    public void vaciarGrafica(){
+        grafica.clear();
+    }
+    
+    /**
+     * metodo que dice quien es el ganador de los 3 candidatos
+     */
+    public void ganador(){
+        int resultado;
+        LogicaVotaciones logica = new LogicaVotaciones();
+        resultado = logica.calcularGanador(getContadorUno(), getContadorDos(), getContadorTres());
+        
+        if (resultado == 1) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "Alvaro Fajardo gana con " +getContadorUno() + " votos");       
+            PrimeFaces.current().dialog().showMessageDynamic(message);
+            init();
+            vaciarGrafica();
+        } else if (resultado == 2) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "Diego Garzon gana con " +getContadorDos()+ " votos");       
+            PrimeFaces.current().dialog().showMessageDynamic(message);
+            init();
+            vaciarGrafica();
+        } else if (resultado == 3) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "Guillermo Aldana gana con " +getContadorTres()+ " votos");       
+            PrimeFaces.current().dialog().showMessageDynamic(message);
+            init();
+            vaciarGrafica();
+        } else {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje", "Empate");       
+            PrimeFaces.current().dialog().showMessageDynamic(message);
+            init();
+            vaciarGrafica();
+        }            
+    }
 
+    /**
+     * metodos publicos  y privados para acceder a variables encapsuladas
+     * @return 
+     */
     public int getContadorUno() {
         return contadorUno;
     }
 
+    
+    
     public void setContadorUno(int contadorUno) {
         this.contadorUno = contadorUno;
     }
--- src/main/java/com/udec/modelo/LogicaVotaciones.java
+++ src/main/java/com/udec/modelo/LogicaVotaciones.java
@@ -7,17 +7,34 @@ package com.udec.modelo;
 
 /**
  *
- * @author michl
+ * @author Michael Cardenas
+ * clase encargada de hallar el ganador y devolverselo al controlador
  */
 public class LogicaVotaciones {
    
+    /**
+     * constructor vacio de la clase
+     */
+    
     public LogicaVotaciones(){
         
     }
     
-    
-    public void calcularGanador(){
+    /**
+     * metodo que calcula el ganador basandose en los votos finales de cada candidato
+     * @param a guarda el numero de votos del candidato 1
+     * @param b guarda el numero de votos del candidato 2
+     * @param c guarda el numero de votos del candidato 3
+     * @return devuelve el ganador de los 3 candidatos
+     */
+    public int calcularGanador(int a,int b, int c){
+        if(a>b&&a>c)
+            return 1;
+        else if(b>a&&b>c)
+            return 2;
+        else if(c>a&&c>b)
+            return 3;
+        else 
+            return 0;
     }
-    
-    
 }
